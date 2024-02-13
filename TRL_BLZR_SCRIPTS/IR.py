b55f9d922ead9a8faaa7b56e5c732698333e6018
Andus Polancus <125496310+Andyp1352@users.noreply.github.com>
2024-02-12 20:07:35 -0600

    Add files via upload
    Automatic IR footage storage for Raspberry Pi camera.

---
+++ TRL_BLZR_SCRIPTS/IR.py
@@ -0,0 +1,15 @@
+from picamera import PiCamera 
+import keyboard
+
+#Script for automatic IR footage storage. This will create a single file of video
+#and will overwrite previous file once this script is ran again. If we intend to
+#store every single flight video, then I propose the creation of a naming standard.
+
+camera.start_preview() #Create a preview window
+camera.start_recording('/home/Desktop/video.h264') #Starts recording of IR camera, and saves in given path once done
+
+input("Press enter key to stop recording...")      #Wait for user to hit enter 
+
+camera.stop_recording()                            #Once user has hit enter, stop recording
+camera.stop_preview                                #Kill preview window
+print("VIDEO HAS BEEN SAVED AS: video.h264")       #Give user name of video created
