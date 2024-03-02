c42366f9f1b4af3a3c3a18d4c7c055a5b98978d1
Zachary Evans <prescient.slime@gmail.com>
2024-03-02 13:14:05 -0600

    checkpoint

--- TRL_BLZR_PATH/demo_flight.py
+++ TRL_BLZR_PATH/demo_flight.py
@@ -2,12 +2,28 @@
 
 import asyncio
 from mavsdk import System
-
-drone = System()
+from mavsdk.mission import MissionItem, MissionPlan
 
 async def run():
-    print("trying to connect to drone...")
+    drone = System()
     await drone.connect(system_address="serial:///dev/ttyAMA0:57600")
+    print("Waiting for drone to connect...\n")
+
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("-- Connected to drone!")
+            break
+
+    print_mission_progress_task = asyncio.ensure_future(print_mission_progress(drone))
+
+    running_tasks = [print_mission_progress_task]
+    termination_task = asyncio.ensure_future(
+        observe_is_in_air(drone, running_tasks)
+    )
+
+    mission_items = []
+    mission_items.append(MissionItem())
+    
 
 if __name__ == "__main__":
     asyncio.run(run())
