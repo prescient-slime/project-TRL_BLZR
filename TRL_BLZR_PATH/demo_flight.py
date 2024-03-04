a71677899aca2bfcb1f8d99435e0aaa0936802fb
Zachary Evans <prescient.slime@gmail.com>
2024-03-03 18:19:07 -0600

    please god just work

--- TRL_BLZR_PATH/demo_flight.py
+++ TRL_BLZR_PATH/demo_flight.py
@@ -1,104 +1,22 @@
-#!/usr/bin/env python3
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+import time
 
-import asyncio
+vehicle = connect("/dev/ttyAMA0", wait_ready=True, baud=57600)
 
-from mavsdk import System
-from mavsdk.mission import (MissionItem, MissionPlan)
+vehicle.mode = VehicleMode("GUIDED")
 
-print("Imports Successful")
+vehicle.armed = True
 
-async def run():
-    drone = System()
-    await drone.connect(system_address="serial:///dev/ttyAMA0:57600")
+vehicle.simple_takeoff(3)
 
-    print("Waiting for drone to connect...")
-    async for state in drone.core.connection_state():
-        if state.is_connected:
-            print("-- Connected to drone!")
-            break
+while float(vehicle.location.global_relative_frame.alt) < (3 * 0.95):
+    print(f"Altitude = {vehicle.location.global_relative_frame.alt}")
+    if vehicle.location.global_relative_frame.alt == (3 * 0.95):
+        print("Target altitude reached")
+    time.sleep(1)
 
-    print_mission_progress_task = asyncio.ensure_future(
-        print_mission_progress(drone))
+print("Landing")
+vehicle.mode = VehicleMode("LAND")
 
-    running_tasks = [print_mission_progress_task]
-    termination_task = asyncio.ensure_future(
-        observe_is_in_air(drone, running_tasks))
-
-    #waypoint array
-    mission_items = []
-    mission_items.append(MissionItem(34.98102529395546,
-                                     -101.91514572067254,
-                                     9,
-                                     5,
-                                     False,
-                                     float('nan'),
-                                     float('nan'),
-                                     MissionItem.CameraAction.NONE,
-                                     float(5.0),
-                                     float('nan'),
-                                     float(2),
-                                     float('nan'),
-                                     float('nan'),
-                                     0))
-    #assemble waypoint array into MissionPlan object
-    mission_plan = MissionPlan(mission_items)
-
-    #Return to launch point after flying to waypoint
-    await drone.mission.set_return_to_launch_after_mission(True)
-
-    #Upload mission to drone
-    print("-- Uploading mission")
-    await drone.mission.upload_mission(mission_plan)
-
-    #Receive drone telemetry
-    print("Waiting for drone to have a global position estimate...")
-    async for health in drone.telemetry.health():
-        if health.is_global_position_ok and health.is_home_position_ok:
-            print("-- Global position estimate OK")
-            break
-    
-    #arm drone
-    print("-- Arming")
-    await drone.action.arm()
-
-    #begin drone mission from mission_plan
-    print("-- Starting mission")
-    await drone.mission.start_mission()
-    
-    #wait for drone to stop
-    await termination_task
-
-
-async def print_mission_progress(drone):
-    async for mission_progress in drone.mission.mission_progress():
-        print(f"Mission progress: "
-              f"{mission_progress.current}/"
-              f"{mission_progress.total}")
-
-
-async def observe_is_in_air(drone, running_tasks):
-    """ Monitors whether the drone is flying or not and
-    returns after landing """
-
-    was_in_air = False
-
-    async for is_in_air in drone.telemetry.in_air():
-        if is_in_air:
-            was_in_air = is_in_air
-
-        if was_in_air and not is_in_air:
-            for task in running_tasks:
-                task.cancel()
-                try:
-                    await task
-                except asyncio.CancelledError:
-                    pass
-            await asyncio.get_event_loop().shutdown_asyncgens()
-
-            return
-
-
-if __name__ == "__main__":
-    # Run the asyncio loop
-    asyncio.run(run())
-
+vehicle.close()
